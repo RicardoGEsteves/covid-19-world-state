a9d6d6f1e5628bc5fa0d0cf65591bf6ccf8ac839
Ricardo Esteves <you@example.com>
2020-03-29 21:09:49 +0100

    Updates

---
+++ precache-manifest.009a3deca4493246e66538496afe7cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c46b927dfce689344e3913ee8c1eee",
+    "url": "/covid-19-world-state/index.html"
+  },
+  {
+    "revision": "0ef66961c67a6b62ca8c",
+    "url": "/covid-19-world-state/static/css/main.6413fe9c.chunk.css"
+  },
+  {
+    "revision": "396505108e8272430083",
+    "url": "/covid-19-world-state/static/js/2.5ba9d998.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-world-state/static/js/2.5ba9d998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef66961c67a6b62ca8c",
+    "url": "/covid-19-world-state/static/js/main.89244eab.chunk.js"
+  },
+  {
+    "revision": "9da7dc51cebfdcbbaec4",
+    "url": "/covid-19-world-state/static/js/runtime-main.535b7fba.js"
+  },
+  {
+    "revision": "7225a689e9c459294241ce9ae1b340b3",
+    "url": "/covid-19-world-state/static/media/logo.7225a689.png"
+  }
+]);
